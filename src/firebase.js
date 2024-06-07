39f50368828dbe18b315e8d2a4e1ee125e84c221
Sahil Kalkal <sahilkalkal108@gmail.com>
2024-06-07 17:16:49 +0530

    Added env in gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -3,16 +3,15 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyAyukm8Xi3MYXSLfAFhseqnf-Uj5G1XS4E",
-    authDomain: "brightspark-6f72f.firebaseapp.com",
-    projectId: "brightspark-6f72f",
-    storageBucket: "brightspark-6f72f.appspot.com",
-    messagingSenderId: "624037381349",
-    appId: "1:624037381349:web:8028219065f6e5b917800c"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+});
 
-})
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
-
-export { auth, firestore }; 
+export { auth, firestore };
